15131a4712376abaaf04b39bcb2c052aca1499cf
sergeiovs <mr.serz98@mail.ru>
2022-09-14 17:46:44 -0700

    123

---
+++ Scripts/My.py
@@ -0,0 +1,6 @@
+list = ['privet' , '' , '' , 'test']
+for i in list:
+    if i == '' :
+        print('empty')
+    
+    print()
---
+++ Scripts/test.py
@@ -0,0 +1,35 @@
+file = open('test.txt','r')
+timefile = open('data.txt','w')
+file1=file.readlines()
+
+datelist =[]
+iplist =  []
+resultList=[]
+maclist = []
+
+for i in file1:
+    
+    date , time , ip , result , mac = i.split(' ')[0] + "'" ,  "'" + i.split(' ')[1] , i.split(',')[1] , i.split(',')[2],i.split(',')[3]
+    
+    
+    #print('|','date = ' , date + '   |   ','time : ' + time , ' | ','ip = ' , ip, '   |   ','result : ' , result , '   |   ','mac : ', mac,'|')
+    
+    datefile = open('data.txt','a+')
+    datefile.write( 'date = '+ str(date) + ',' + 'time' + str(time) + ',' +'ip = ' + str(ip) +','+'res = ' + str(result) + ',' + 'mac = ' + str(mac) )
+
+    #print('ip0 = ' + ip.split('.')[0] + "'" , 'ip0 = ' , ip.split('.')[1] + "'" , 'ip0 = ' + ip.split('.')[2] , "'")
+    print()
+    print()
+    
+    datelist.append(date)
+    iplist.append(ip)
+    resultList.append(result)
+    maclist.append(mac)
+
+
+print( len(datelist) , len(iplist) , len(resultList) , len(maclist) )
+print(datelist)
+
+print('-----')
+print('DONE!')
+print('-----')
